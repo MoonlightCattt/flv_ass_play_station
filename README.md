850b124b87880ecedcb9de18a12c61094f03416a
MoonlightCattt <superloli@foxmail.com>
2023-03-28 21:49:41 +0800

    生而为人类智能，我很抱歉

---
+++ README.md
@@ -0,0 +1,47 @@
+# flv_ass_play_station
+
+## 随时播放你的flv直播录像与弹幕
+
+### 简介
+
+[dplayer](https://dplayer.diygod.dev/)+[ass](https://github.com/weizhenye/ASS)+[flv](https://github.com/Bilibili/flv.js/)=本项目
+
+本项目能够让你随时随地观看带有带有弹幕的偶像直播回放。
+
+### demo
+
+地址：[满舰饰假子直播回放 (zaojique.org)](http://zaojique.org/)
+
+存在缺点：不能一次性准确地在播放器上显示视频大小，但不影响正常使用。只需点击一下全屏即可解决。
+
+### 安装
+
+配置好你的服务器（并且安装好nginx或其他）后，将release放到  */www/wwwroot/149.28.238.5(写你自己的，这是我的)/* 路径中解压好就能用了
+
+![](\README\1.PNG)
+
+当然，现在还没有视频，你可以[配置好B站录播姬](https://rec.danmuji.org/user/install/cli-systemd/) ，
+
+![](\README\2.PNG)
+
+录播姬工作目录设置在 */www/wwwroot/149.28.238.5(写你自己的，这是我的)/video/* 中，酌情设置弹幕并把文件名命名设置为
+
+```
+{{ "now" | time_zone: "Asia/Shanghai" | format_date: "yyyyMMdd" }}.flv
+```
+
+如果工作正常，在主播直播结束后会在前面提到的video文件夹下。同时还会有xml弹幕文件。你可以[酌情](https://rec.danmuji.org/user/other-projects/#%E5%BC%B9%E5%B9%95%E6%96%87%E4%BB%B6%E5%A4%84%E7%90%86%E5%B7%A5%E5%85%B7)把xml转换成ass以供播放。
+
+使用其他录制软件的话，只需要flv文件保存在video文件夹中并以日期命名（比如20230328.flv）就行
+
+### 功能描述
+
+网页会显示带有昨天、前天和大前天日期的视频链接，点开就能看。
+
+待办项：能够被设置为定时执行的删除旧视频旧弹幕并将新xml转换为ass的脚本
+
+### 另
+
+有编译好的linux弹幕工厂，有需要自取。
+
+
---
+++ js/main.js
@@ -0,0 +1,90 @@
+var the_title = '满舰饰假子直播回放 '
+var now = new Date();
+var ms = now.getTime() - timems;
+var timed = new Date(ms);
+var year = timed.getFullYear();
+var month = timed.getMonth() + 1;
+var date = timed.getDate();
+var full_time = year * 10000 + month * 100 + date;
+document.getElementById("top_title").innerHTML = the_title + full_time;
+var video_url = '../video/' + full_time + '.flv';
+var subtitle_url = '../video/' + full_time + '.ass';
+
+window.__DATA__ = {
+	video: {
+		url: video_url,
+		type: 'flv',
+	},
+	pic: null,
+	subtitle: {
+		url: subtitle_url,
+		type: 'ass'
+	}
+};
+
+window.__INIT__ = {};
+
+(function() {
+	window.__INIT__.dPlayer = new DPlayer({
+		container: document.getElementById('dplayer'),
+		screenshot: true,
+		theme: '#66CCFF',
+		playbackSpeed: [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2],
+		subtitle: window.__DATA__.subtitle,
+		video: {
+			url: window.__DATA__.video.url,
+			type: window.__DATA__.video.type,
+			pic: window.__DATA__.pic,
+		},
+		pluginOptions: {
+			flv: {
+				mediaDataSource: {},
+				config: {},
+			},
+		},
+	});
+
+	window.__INIT__.dPlayer.on('subtitle_show', function() {
+		window.__INIT__.dPlayer.notice("显示弹幕", 2000);
+		console.log('subtitle_show');
+		if (window.__INIT__.ass !== undefined) {
+			window.__INIT__.ass.show();
+		}
+	});
+	window.__INIT__.dPlayer.on('subtitle_hide', function() {
+		window.__INIT__.dPlayer.notice("隐藏弹幕", 2000);
+		if (window.__INIT__.ass !== undefined) {
+			window.__INIT__.ass.hide();
+		}
+	});
+
+	window.__INIT__.dPlayer.on('resize', function() {
+		if (window.__INIT__.ass !== undefined) {
+			document.getElementsByClassName('ASS-container')[0].style.width = '';
+			document.getElementsByClassName('ASS-container')[0].style.height = '';
+			window.__INIT__.ass.resize();
+		}
+	});
+	window.onresize = function() {
+		window.__INIT__.dPlayer.resize();
+	};
+
+	window.__INIT__.dPlayer.on('subtitle_change', function() {
+		if (window.__INIT__.dPlayer !== undefined && window.__INIT__.dPlayer.subtitle.options.type ===
+			'ass') {
+			axios.get(window.__INIT__.dPlayer.subtitle.options.url).then(function(response) {
+				if (window.__INIT__.ass !== undefined) {
+					window.__INIT__.ass.destroy();
+				}
+				window.__INIT__.ass = new ASS(response.data, window.__INIT__.dPlayer.video, {
+					container: window.__INIT__.dPlayer.container.getElementsByClassName(
+						'dplayer-video-wrap')[0],
+				});
+			}).catch(function(error) {
+				window.__INIT__.dPlayer.notice("外挂字幕加载失败: " + error, 6000);
+			});
+		}
+	});
+	window.__INIT__.dPlayer.events.trigger('subtitle_change');
+})();
+console.log(window.__INIT__.dPlayer.plugins.flv);
